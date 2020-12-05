abea5894ece35e402e5788e85ddaf497070ee1d5
cikpak <simion.gorgos@gmail.com>
2020-12-05 23:35:01 +0200

    ADD:  auto install script

---
+++ install.sh
@@ -0,0 +1,40 @@
+# exit on any error
+set -e
+
+echo "---Instaling Zerotier-One---"
+# install zerotier
+curl -s https://install.zerotier.com | bash 
+echo "Success!"
+
+
+#Install npm packages
+echo "---Installing project dependencies---"
+npm install
+echo "Success!"
+
+
+#create service
+echo "---Creating xplay service---"
+# create iperf service.
+echo "[[Unit]
+Description=XPlay-personal raspberry server
+After=network.target
+
+[Service]
+Type=simple
+User=pi
+ExecStart=/usr/bin/node /home/pi/xplay-personal/server.js
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/xplay-server.service
+
+echo "Success!"
+
+
+
+# enable & start service
+echo "---Enabling and starting XPlay service---"
+sudo systemctl enable xplay-server.service && sudo systemctl start xplay-server.service
+echo "Success!"
